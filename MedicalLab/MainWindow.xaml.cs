d42de56f04d2d324867bfc0ee864d74efbe19211
Maciej Biazik <Maciej.Biazik@microsoft.wsei.edu.pl>
2022-06-06 19:48:45 +0200

    Bind first data grid

--- MedicalLab/MainWindow.xaml.cs
+++ MedicalLab/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MedicalLab.Data;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MedicalLab
 {
@@ -20,14 +12,30 @@ namespace MedicalLab
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MedicalLabContext _context = new MedicalLabContext();
+        private CollectionViewSource patientViewSource;
+
         public MainWindow()
         {
             InitializeComponent();
+            patientViewSource = (CollectionViewSource)FindResource(nameof(patientViewSource));
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _context.Patients.Load();
+            patientViewSource.Source = _context.Patients.Local.ToObservableCollection();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _context.Dispose();
+            base.OnClosing(e);
         }
     }
 }

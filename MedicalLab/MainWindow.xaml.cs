86442be70cadfa5a8f0ae38945a3230417b54d80
Maciej Biazik <Maciej.Biazik@microsoft.wsei.edu.pl>
2022-06-09 18:51:31 +0200

    Bind Tester ComboBox

--- MedicalLab/MainWindow.xaml.cs
+++ MedicalLab/MainWindow.xaml.cs
@@ -13,12 +13,14 @@ namespace MedicalLab
     public partial class MainWindow : Window
     {
         private readonly MedicalLabContext _context = new MedicalLabContext();
+        private CollectionViewSource testerViewSource;
         private CollectionViewSource patientViewSource;
 
         public MainWindow()
         {
             InitializeComponent();
             patientViewSource = (CollectionViewSource)FindResource(nameof(patientViewSource));
+            testerViewSource = (CollectionViewSource)FindResource(nameof(testerViewSource));
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -29,7 +31,9 @@ namespace MedicalLab
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             _context.Patients.Load();
+            _context.Testers.Load();
             patientViewSource.Source = _context.Patients.Local.ToObservableCollection();
+            testerViewSource.Source = _context.Testers.Local.ToObservableCollection();
         }
 
         protected override void OnClosing(CancelEventArgs e)
